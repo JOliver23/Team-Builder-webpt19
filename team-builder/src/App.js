0e80f4cc4eab1e60f547f1edbb66d17baf468c1f
JOliver23 <59593045+JOliver23@users.noreply.github.com>
2020-08-07 18:38:50 -0700

    set up state; form built; new members render to DOM

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,23 +1,30 @@
 import React, { useState } from 'react';
 import MemberCard from './components/MemberCard';
+import NewMemberForm from './components/NewMemberForm';
 
 function App() {
   const [team, setNewMember] = useState([
     {
       id: 1,
       name: "Bubba Mooseford",
-      age: "25",
+      email: "B.MooseFord@msn.com",
       role: "HR Morale Booster"
     },
     {
       id: 2,
       name: "Wendy Ibber",
-      age: "29",
+      email: "wendy.ibbs@msn.com",
       role: "Fun Inspector"
     }
-  ])
+  ]);
+
+  const addNewMember = (member) => {
+    setNewMember([...team, {...member, id: Date.now() }])
+  };
+
   return (
     <div className="App">
+      <NewMemberForm setNewMember={addNewMember}  member={team} />
       <MemberCard team={team} />
     </div>
   );
--- team-builder/src/components/MemberCard.js
+++ team-builder/src/components/MemberCard.js
@@ -7,7 +7,7 @@ const MemberCard = props => {
             {props.team.map(member => (
                 <div className="memberCard" key ={member.id}>
                     <h2>{member.name}</h2>
-                    <span>{member.age}</span>
+                    <p>{member.email}</p>
                     <p>{member.role}</p>
                 </div>
             ))}
